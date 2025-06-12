8a92eed5d8a460b4134d147a6687313d2d322f50
Milan Radijator <milan.stankovic@radijator.rs>
2025-06-12 14:03:56 +0200

    * Komitujem sve izmene dodajem i sastavnice

--- app/Exports/WorkOrderItemsExport.php
+++ app/Exports/WorkOrderItemsExport.php
@@ -4,8 +4,10 @@ namespace App\Exports;
 
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+use Maatwebsite\Excel\Concerns\WithDrawings;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
 
-class WorkOrderItemsExport implements FromView
+class WorkOrderItemsExport implements FromView, WithDrawings
 {
     protected $workOrder;
 
@@ -18,7 +20,29 @@ class WorkOrderItemsExport implements FromView
     {
         return view('exports.work-order-items', [
             'workOrder' => $this->workOrder,
-            'items' => $this->workOrder->items, // relacija sa WorkOrderItem modelima
+            'items' => $this->workOrder->items()->with(['workPhase', 'product'])->get(),
         ]);
+    }
+    public function drawings()
+    {
+        $drawings = [];
+
+        foreach ($this->workOrder->items as $index => $item) {
+            $drawing = new Drawing();
+            $drawing->setName('Logo ' . ($index + 1));
+            $drawing->setDescription('Radijator Inženjering - ' . ($index + 1));
+            $drawing->setPath(public_path('logo.png'));
+            $drawing->setHeight(90);
+
+            // Primer: stavljaš u kolonu F i redove 1, 30, 60, itd.
+            $row = 2 + ($index * 22); // Pretpostavka: svaki zapis zauzima oko 30 redova
+            $drawing->setCoordinates('E' . $row);
+            $drawing->setOffsetX(10);
+            $drawing->setOffsetY(5);
+
+            $drawings[] = $drawing;
+        }
+
+        return $drawings;
     }
 }
--- resources/views/exports/work-order-items.blade.php
+++ resources/views/exports/work-order-items.blade.php
@@ -1,90 +1,148 @@
 @foreach ($items as $item)
-<table border="1" cellspacing="0" cellpadding="5" style="border-collapse:collapse; width:100%; font-size:12px;">
-    {{-- Naslov i gornji deo --}}
+<table border="1" cellspacing="0" cellpadding="5" style="border-collapse: collapse; width: 100%; font-size: 12px;">
+    <colgroup>
+        <col style="width: 100px;">
+        <col style="width: 100px;">
+        <col style="width: 80px;">
+        <col style="width: 110px;">
+        <col style="width: 40px;">
+        <col style="width: 110px;">
+        <col style="width: 50px;">
+        <col style="width: 50px;">
+        <col style="width: 50px;">
+        <col style="width: 55px;">
+        <col style="width: 80px;">
+        <col style="width: 60px;">
+        <col style="width: 100px;">
+    </colgroup>
+
     <tr>
-        <td colspan="4" rowspan="4" style="background-color:#CCFFCC;">
-            <strong>RADNA LISTA-IZVEŠTAJ O RADU</strong><br>
-            Linija montaže br:<br>
-            Ime i prezime radnika/potpis:<br>
-            Matični br. radnika:
+        <td colspan="13" style="background-color: #EEEEEE; text-align: left; font-weight: bold; font-size: 14px; border: 2px solid #999;">
+            STAVKA {{ $loop->iteration }}
         </td>
-        <td colspan="2" style="text-align:center;" rowspan="4">
-            <img src="{{ public_path('logo.png') }}" width="150">
-        </td>
-        <td><strong>RN BR:</strong></td>
-        <td>{{ $workOrder->full_name }}</td>
-    </tr>
-    <tr>
-        <td><strong>Kotao:</strong></td>
-        <td>{{ $item->product_name ?? 'N/A' }}</td>
-    </tr>
-    <tr>
-        <td><strong>Datum:</strong></td>
-        <td>{{ \Carbon\Carbon::parse($workOrder->launch_date)->format('d.m.Y') }}</td>
-    </tr>
-    <tr>
-        <td><strong>Količina:</strong></td>
-        <td>{{ $workOrder->quantity }}</td>
     </tr>
 
-    {{-- Operacija --}}
-    <tr style="background-color:#CCFFCC;">
-        <td colspan="2"><strong>OPERACIJA BR</strong></td>
-        <td colspan="2">{{ $item->operation_number ?? '' }}</td>
-        <td><strong>Tprz[min]</strong></td>
-        <td>&nbsp;</td>
-        <td colspan="2"><strong>urađena količina</strong></td>
-    </tr>
     <tr>
-        <td colspan="2"><strong>Šifra posla</strong></td>
-        <td><strong>Tk[min]</strong></td>
-        <td>{{ $item->time_norm ?? '' }}</td>
-        <td><strong>datum</strong></td>
-        <td colspan="3">&nbsp;</td>
-    </tr>
-    <tr>
-        <td colspan="2"><strong>Br.radnika</strong></td>
-        <td>{{ $item->number_of_workers ?? '' }}</td>
-        <td><strong>Vreme početka</strong></td>
-        <td colspan="4">&nbsp;</td>
-    </tr>
-    <tr>
-        <td colspan="3">&nbsp;</td>
-        <td><strong>Vreme završetka</strong></td>
-        <td colspan="4">&nbsp;</td>
+        <td colspan="3" style="background-color: #CCFFCC; font-weight: bold; border: 2px solid black;">RADNA LISTA-IZVEŠTAJ O RADU</td>
+        <td colspan="7" rowspan="5" style="border: 2px solid black;"></td>
+        <td style="background-color: #CCFFCC; font-weight: bold; border: 2px solid black;">RN BR:</td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;">{{ $workOrder->full_name }}</td>
     </tr>
 
-    {{-- Opis operacije --}}
     <tr>
-        <td colspan="8"><strong>{{ $item->operation_description ?? 'Pakovanje sklopa priključaka (sa zadnje strane)' }}</strong></td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;">Linija montaže br:</td>
+        <td style="font-weight: bold; border: 2px solid black;"></td>
+        <td style="background-color: #CCFFCC; font-weight: bold; border: 2px solid black;">Kotao:</td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;">{{ $item->product->name ?? 'N/A' }}</td>
     </tr>
 
-    {{-- Kontrola --}}
-    <tr style="background-color:#CCFFCC;">
-        <td colspan="2"><strong>OPERACIJA</strong></td>
-        <td colspan="2">&nbsp;</td>
-        <td><strong>obim kontrole</strong></td>
-        <td>100%</td>
-        <td>50%</td>
-        <td>10% uzork</td>
-    </tr>
     <tr>
-        <td colspan="2"><strong>Kontrola</strong></td>
-        <td colspan="2">&nbsp;</td>
-        <td><strong>datum</strong></td>
-        <td colspan="3">&nbsp;</td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;">Ime i prezime radnika / potpis:</td>
+        <td style="font-weight: bold; border: 2px solid black;"></td>
+        <td style="background-color: #CCFFCC; font-weight: bold; border: 2px solid black;">Datum:</td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;">{{ \Carbon\Carbon::parse($workOrder->launch_date)->format('d.m.Y') }}</td>
     </tr>
+
     <tr>
-        <td colspan="2"><strong>Izveštaj o doradi br:</strong></td>
-        <td colspan="2">1xRadnik</td>
-        <td><strong>kontrolor</strong></td>
-        <td colspan="3">&nbsp;</td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;">Matični br. radnika:</td>
+        <td style="font-weight: bold; border: 2px solid black;"></td>
+        <td style="background-color: #CCFFCC; font-weight: bold; border: 2px solid black;">Količina:</td>
+        <td colspan="2" style="font-weight: bold; text-align: left; border: 2px solid black;">{{ $workOrder->quantity }}</td>
     </tr>
+
+    <tr>@for ($i = 0; $i < 13; $i++) <td ></td> @endfor</tr>
+    <tr>@for ($i = 0; $i < 13; $i++) <td ></td> @endfor</tr>
+
     <tr>
-        <td colspan="2"><strong>Izveštaj o škartu br:</strong></td>
-        <td colspan="2">1xRJ</td>
-        <td colspan="4">&nbsp;</td>
+        <td colspan="3" style="background-color: #CCFFCC; font-weight: bold; border: 2px solid black;">OPERACIJA BR: {{ $loop->iteration * 10 }}</td>
+        <td style="font-weight: bold; border: 2px solid black;">Tprz[min]</td>
+        <td style="font-weight: bold; border: 2px solid black;"></td>
+        <td style="font-weight: bold; border: 2px solid black;">Urađena količina</td>
+        <td colspan="7" style="font-weight: bold; border: 2px solid black;"></td>
+    </tr>
+
+    <tr>
+        <td style="font-weight: bold; border: 2px solid black;">Šifra posla</td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;"></td>
+        <td style="font-weight: bold; border: 2px solid black;">Tk[min]</td>
+        <td style="font-weight: bold; border: 2px solid black;">{{ $item->workPhase->time_norm }}</td>
+        <td style="font-weight: bold; border: 2px solid black;">Datum</td>
+        <td colspan="7" style="font-weight: bold; border: 2px solid black;"></td>
+    </tr>
+
+    <tr>
+        <td style="font-weight: bold; border: 2px solid black;">Br. radnika</td>
+        <td style="font-weight: bold; border: 2px solid black;">{{ $item->workPhase->number_of_workers }}</td>
+        <td style="font-weight: bold; border: 2px solid black;"></td>
+        <td style="font-weight: bold; border: 2px solid black;">Vreme početka</td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;"></td>
+        <td colspan="7" style="font-weight: bold; border: 2px solid black;"></td>
+    </tr>
+
+    <tr>
+        <td colspan="3"></td>
+        <td style="font-weight: bold; border: 2px solid black;">Vreme završetka</td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;"></td>
+        <td colspan="7" style="font-weight: bold; border: 2px solid black;"></td>
+    </tr>
+
+    <tr>
+        <td colspan="13" style="font-weight: bold; font-size: 14px; border: 2px solid black;">{{ $item->workPhase->name }}</td>
+    </tr>
+
+    <tr>@for ($i = 0; $i < 13; $i++) <td style="font-weight: bold; border: 2px solid black;"></td> @endfor</tr>
+    <tr>@for ($i = 0; $i < 13; $i++) <td style="font-weight: bold; border: 2px solid black;"></td> @endfor</tr>
+
+    <tr>
+        <td colspan="3" style="background-color: #CCFFCC; font-weight: bold; border: 2px solid black;">OPERACIJA</td>
+        <td colspan="3" style="font-weight: bold; border: 2px solid black;"></td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;">OBIM KONTROLE</td>
+        <td style="font-weight: bold; border: 2px solid black;">100%</td>
+        <td style="font-weight: bold; border: 2px solid black;">50%</td>
+        <td style="font-weight: bold; border: 2px solid black;">10%</td>
+        <td style="font-weight: bold; border: 2px solid black;">UZROK</td>
+        <td style="font-weight: bold; border: 2px solid black;">OSTALO</td>
+    </tr>
+
+    <tr>
+        <td style="font-weight: bold; border: 2px solid black;">KONTROLA</td>
+        <td colspan="5" style="font-weight: bold; border: 2px solid black;"></td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;">Datum</td>
+        <td colspan="1" style="font-weight: bold; border: 2px solid black;"></td>
+        <td colspan="1" style="font-weight: bold; border: 2px solid black;"></td>
+        <td colspan="1" style="font-weight: bold; border: 2px solid black;"></td>
+        <td colspan="1" style="font-weight: bold; border: 2px solid black;"></td>
+        <td colspan="1" style="font-weight: bold; border: 2px solid black;"></td>
+    </tr>
+
+    <tr>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;">Izveštaj o doradi br:</td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;"></td>
+        <td style="font-weight: bold; border: 2px solid black;"></td>
+        <td style="font-weight: bold; border: 2px solid black;">1xRadnik</td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;">Kontrolor</td>
+        <td colspan="1" style="font-weight: bold; border: 2px solid black;"></td>
+        <td colspan="1" style="font-weight: bold; border: 2px solid black;"></td>
+        <td colspan="1" style="font-weight: bold; border: 2px solid black;"></td>
+        <td colspan="1" style="font-weight: bold; border: 2px solid black;"></td>
+        <td colspan="1" style="font-weight: bold; border: 2px solid black;"></td>
+    </tr>
+
+    <tr>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;">Izveštaj o škartu br:</td>
+        <td colspan="2" style="font-weight: bold; border: 2px solid black;"></td>
+        <td style="font-weight: bold; border: 2px solid black;"></td>
+        <td style="font-weight: bold; border: 2px solid black;">1xRJ</td>
+        <td colspan="7" style="font-weight: bold; border: 2px solid black;"></td>
+    </tr>
+
+    <tr>@for ($i = 0; $i < 13; $i++) <td style="font-weight: bold; border: 2px solid black;"></td> @endfor</tr>
+
+    <tr>
+        <td colspan="3" style="font-weight: bold; border: 2px solid black;"></td>
+        <td style="font-weight: bold; border: 2px solid black;">Poslovođa</td>
+        <td colspan="9" style="font-weight: bold; border: 2px solid black;"></td>
     </tr>
 </table>
-<br><br>
+<br>
 @endforeach

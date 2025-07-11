ca5b98aa6a492a102136da5a11240f4bd14ca1c6
Milan Radijator <milan.stankovic@radijator.rs>
2025-07-11 13:45:02 +0200

    * ispravka

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-const isProduction = process.env.NODE_ENV === 'production';
-
 export default defineConfig({
     base: '/build/',
     server: {
-        host: '127.0.0.1',
+        host: 'localhost',
         port: 5173,
-        https: isProduction,
     },
     plugins: [
         laravel({
